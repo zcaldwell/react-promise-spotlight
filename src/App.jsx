96e37802914e825e41df5ac1330c57eb64845d55
zcaldwell <mintyst.john@gmail.com>
2022-01-25 11:56:19 -0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from 'react';
+import {
+  getMachines,
+  getPinballMachines,
+  getAnime,
+  getAnimeInfo,
+} from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    getMachines();
+  });
+
+  useEffect(() => {
+    getAnime();
+  });
   return <h1>Hello World</h1>;
 }
+console.log(getPinballMachines());
+console.log(getAnimeInfo);
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,19 +1,42 @@
 /**
- * TODO:
+ * TODO: Get data from https://ponball.herokuapp.com/api/v1/machines
  */
+export async function getPinballMachines() {
+  const resp = await fetch(`https://ponball.herokuapp.com/api/v1/machines`);
+  const data = await resp.json();
+  return console.log(data);
+}
 
 /**
  * TODO:
  */
 
+export const getMachines = () => {
+  const response = fetch('https://ponball.herokuapp.com/api/v1/machines').then(
+    (data) => data.json().then((machines) => console.log(machines))
+  );
+  return response;
+};
 /**
  * TODO:
  */
+export async function getAnime() {
+  const resp = await fetch(
+    'https://api.jikan.moe/v4/anime?q=yu yu hakusho&sfw'
+  );
+  const data = await resp.json();
+  return console.log(data);
+}
 
 /**
  * TODO:
  */
-
+export const getAnimeInfo = () => {
+  const response = fetch(
+    'https://api.jikan.moe/v4/anime?q=yu yu hakusho&sfw'
+  ).then((data) => data.json().then((anime) => console.log(anime)));
+  return response;
+};
 /**
  * TODO:
  */
